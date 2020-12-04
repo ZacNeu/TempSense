67722acbd05513d077ef49586b794757fc956454
Zac Neuhardt <zneuhardt77@gmail.com>
2020-12-03 19:31:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import subprocess
 import time
+import json
+import requests
+
+key = '3010a4137160bda6d8b4750c2731ec2d'
+url = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid='+key)
+weather = json.loads(url.text)
 
 while True:    
     subprocess.call("./update_temperature.sh")
@@ -12,5 +18,5 @@ while True:
     celsius = int(hex, 16)
     fahrenheit = celsius * 0.8 + 32
     print(timestamp + ": " + str(celsius) + "°C or", str(fahrenheit) + '°F')
-
+    print weather['main']['temp'],"Temperature"
     time.sleep(1)
